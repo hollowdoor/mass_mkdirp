e5f6012698bcf11ee6f41f2ef9bd1de48f340ebc
hollowdoor <hollowdoor99@gmail.com>
2016-01-01 18:43:27 -0600

    Everything's fine.

---
+++ index.js
@@ -0,0 +1,35 @@
+var mkdirp = require('mkdirp-omen'),
+    fsExists = require('fs-promise').exists,
+    pathJoin = require('path').join;
+/* mass-mkdirp */
+function MassDirp(dirs){
+    if(arguments.length){
+        dirs = Array.prototype.slice.call(arguments);
+    }
+
+    dirs = dirs.map(function(dir){
+        return type(dir) === '[object Array]' ?
+               pathJoin.apply(pathJoin, dir) :
+               dir;
+    }).filter(function(dir){
+        return typeof dir === 'string';
+    });
+
+    return Promise.all(dirs.map(function(dir){
+        return fsExists(dir).then(function(exists){
+            if(exists){
+                return dir;
+            }
+
+            return mkdirp(dir).then(function(){
+                return dir;
+            });
+        });
+    }));
+}
+
+module.exports = MassDirp;
+
+function type(o){
+    return Object.prototype.toString.call(o);
+}
